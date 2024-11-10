a647142d5dc1f90972bf4b9c2c9d77b1393a9069
Cynicalexe <82634333+lolnoobzz@users.noreply.github.com>
2024-11-10 16:11:52 +0800

    Update bgms.js

--- bgms.js
+++ bgms.js
@@ -1,13 +1,28 @@
-window.addEventListener('load', function() {
-    
-    if (!document.getElementById('bgm-audio')) {
-      const audio = document.createElement('audio');
-    audio.id = 'background-music';
-    audio.src = 'bgmm.mp3';
-    audio.autoplay = true; 
-    audio.loop = true; 
-    audio.muted = false; 
-    audio.style.display = 'none';
-    document.body.appendChild(audio); 
-    }
-  });
+const audio = document.getElementById('background-music');
+
+
+if (localStorage.getItem("musicPlaying") === "true") {
+  audio.muted = false;
+  audio.play();
+}
+
+
+const lastTime = localStorage.getItem('background-music-time');
+if (lastTime) {
+  audio.currentTime = parseFloat(lastTime);
+}
+
+
+audio.addEventListener('play', () => {
+  localStorage.setItem("musicPlaying", "true");
+  audio.muted = false;
+});
+
+audio.addEventListener('pause', () => {
+  localStorage.setItem("musicPlaying", "false");
+});
+
+
+window.addEventListener('beforeunload', () => {
+  localStorage.setItem('background-music-time', audio.currentTime);
+});
