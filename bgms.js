ea0ab0078abfd2e9b691fc0d05c1fd446179c2e1
Cynicalexe <82634333+lolnoobzz@users.noreply.github.com>
2024-11-10 15:42:13 +0800

    Update bgms.js

--- bgms.js
+++ bgms.js
@@ -1,12 +1,13 @@
 window.addEventListener('load', function() {
     
-    if (!document.getElementById('bgm-iframe')) {
-      const iframe = document.createElement('iframe');
-      iframe.src = 'bgm.html';
-      iframe.style.display = 'none'; 
-      iframe.width = '0'; 
-      iframe.height = '0'; 
-      iframe.id = 'bgm-iframe'; 
-      document.body.appendChild(iframe); 
+    if (!document.getElementById('bgm-audio')) {
+      const audio = document.createElement('audio');
+    audio.id = 'background-music';
+    audio.src = 'bgmm.mp3';
+    audio.autoplay = true; 
+    audio.loop = true; 
+    audio.muted = false; 
+    audio.style.display = 'none';
+    document.body.appendChild(audio); 
     }
   });
